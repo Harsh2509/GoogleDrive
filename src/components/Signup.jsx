20a5135c2a8c6635b6aeb8f86b0afcd26735be73
Harsh <harshchauhan6176@gmail.com>
2024-03-11 08:42:07 +0530

    Designed SignUp Component

---
+++ src/components/Signup.jsx
@@ -0,0 +1,46 @@
+import { Card, Form, Button } from "react-bootstrap";
+import { useRef } from "react";
+
+export const Signup = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Sign Up</h2>
+          <Form>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" required ref={emailRef}></Form.Control>
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                required
+                ref={passwordRef}
+              ></Form.Control>
+            </Form.Group>
+            <Form.Group id="email">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control
+                type="password"
+                required
+                ref={passwordConfirmRef}
+              ></Form.Control>
+            </Form.Group>
+            <Button type="submit" className="w-100">
+              Sign Up
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Already have an account? Log In
+      </div>
+    </>
+  );
+};
