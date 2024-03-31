73b626a0e0580c93928a0f0f22dd97208a479510
Harsh Chauhan <91670984+Harsh2509@users.noreply.github.com>
2024-03-31 09:28:25 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,8 @@
-// import React from "react";
-
-import { AuthProvider } from "../context/AuthContext";
-import { Signup } from "./authentication/Signup";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Login } from "./authentication/Login";
-import { PrivateRoute } from "./authentication/PrivateRoute";
-import Profile from "./authentication/Profile";
-import { ForgotPassword } from "./authentication/ForgotPassword";
-import { Dashboard } from "./google-drive/Dashboard.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/user",
-    element: (
-      <PrivateRoute>
-        <Profile />
-      </PrivateRoute>
-    ),
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/forgot-password",
-    element: <ForgotPassword />,
-  },
-  {
-    path: "/",
-    element: <Dashboard />,
-  },
-  {
-    path: "/folder/:folderId",
-    element: (
-      <PrivateRoute>
-        <Dashboard />
-      </PrivateRoute>
-    ),
-  },
-]);
+import React from "react";
 
 function App() {
   return (
-    <AuthProvider>
-      <RouterProvider router={router}></RouterProvider>
-    </AuthProvider>
+    <div></div>
   );
 }
 
