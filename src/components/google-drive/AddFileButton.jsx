b6982875c6d1d01df4dadc4acda3f7e55e177803
Harsh Chauhan <91670984+Harsh2509@users.noreply.github.com>
2024-03-31 09:38:09 +0530

    Update AddFileButton.jsx

--- src/components/google-drive/AddFileButton.jsx
+++ src/components/google-drive/AddFileButton.jsx
@@ -1,33 +1,8 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFileUpload } from "@fortawesome/free-solid-svg-icons";
-import { fileStorage } from "../../firebase";
-import { useAuth } from "../../context/AuthContext";
-import { ROOT_FOLDER } from "../../hooks/useFolder";
-
 export default function AddFileButton({ currentFolder }) {
-  const { currentUser } = useAuth();
-
-  function handleUpload(e) {
-    const file = e.target.files[0];
-    if (currentFolder == null || file == null) return;
-
-    const filePath =
-      currentFolder === ROOT_FOLDER
-        ? `${currentFolder.path.join("/")}/${file.name}`
-        : `${currentFolder.path.join("/")}/${currentFolder.name}/${file.name}`;
-
-    fileStorage.addFile(filePath, file, currentUser, currentFolder);
-  }
+  
   return (
     <>
-      <label className="btn btn-outline-success btn-sm mx-3 m-0">
-        <FontAwesomeIcon icon={faFileUpload}></FontAwesomeIcon>
-        <input
-          type="file"
-          onChange={handleUpload}
-          style={{ opacity: 0, position: "absolute", left: "-9999px" }}
-        />
-      </label>
+      
     </>
   );
 }
