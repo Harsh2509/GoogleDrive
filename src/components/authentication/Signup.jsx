f3b2bd3d5d3355d1fafc1bf1ad066f9b72a32937
Harsh Chauhan <91670984+Harsh2509@users.noreply.github.com>
2024-03-31 09:39:24 +0530

    Update Signup.jsx

--- src/components/authentication/Signup.jsx
+++ src/components/authentication/Signup.jsx
@@ -1,72 +1,7 @@
-import { Card, Form, Button, Alert } from "react-bootstrap";
-import { useRef, useState } from "react";
-import { useAuth } from "../../context/AuthContext";
-import { Link, useNavigate } from "react-router-dom";
-import { CenteredContainer } from "./CenteredContainer";
-
 export const Signup = () => {
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
-  const { signUp } = useAuth();
-  const navigate = useNavigate();
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    if (passwordConfirmRef.current.value !== passwordRef.current.value) {
-      return setError("Password do not match");
-    }
-
-    try {
-      setError("");
-      setLoading(true);
-      await signUp(emailRef.current.value, passwordRef.current.value);
-      navigate("/");
-    } catch (e) {
-      setError("Failed to create an account");
-      console.error(e);
-    }
-    setLoading(false);
-  }
-
   return (
-    <CenteredContainer>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
-          {error != "" && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" required ref={emailRef}></Form.Control>
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                required
-                ref={passwordRef}
-              ></Form.Control>
-            </Form.Group>
-            <Form.Group id="confirm-password">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control
-                type="password"
-                required
-                ref={passwordConfirmRef}
-              ></Form.Control>
-            </Form.Group>
-            <Button type="submit" className="w-100 mt-4" disabled={loading}>
-              Sign Up
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        Already have an account? <Link to="/login">Log In</Link>
-      </div>
-    </CenteredContainer>
+    <>
+      
+    </>
   );
 };
