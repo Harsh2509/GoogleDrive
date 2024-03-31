b157fa4831ba2e8dfff8648c11885969459e78bb
Harsh Chauhan <91670984+Harsh2509@users.noreply.github.com>
2024-03-31 09:41:26 +0530

    Update PrivateRoute.jsx

--- src/components/authentication/PrivateRoute.jsx
+++ src/components/authentication/PrivateRoute.jsx
@@ -1,15 +1,3 @@
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthContext";
-import { useEffect } from "react";
-
 export const PrivateRoute = ({ children }) => {
-  const { currentUser } = useAuth();
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!currentUser) {
-      navigate("/login", { replace: true });
-    }
-  }, [currentUser, navigate]);
-
-  return children;
+  return <></>
 };
