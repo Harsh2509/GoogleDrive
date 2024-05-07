c1a60f0f8ed480cabf6553974e136e19ef7949e2
Harsh Chauhan <91670984+Harsh2509@users.noreply.github.com>
2024-05-07 12:53:29 +0530

    Update CenteredContainer.jsx

--- src/components/authentication/CenteredContainer.jsx
+++ src/components/authentication/CenteredContainer.jsx
@@ -1,7 +1,14 @@
+import { Container } from "react-bootstrap";
+
 export const CenteredContainer = ({ children }) => {
   return (
-    <>
-      
-    </>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ height: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        {children}
+      </div>
+    </Container>
   );
 };
